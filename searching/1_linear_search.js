16e80fff97a20930b811b72ae70c9eb60af7b4ba
Hadi Gunawan Tambunan <hadigunawantambunan@JKT-MAC-2388.local>
2025-02-26 20:30:34 +0700

    add linear search

--- searching/1_linear_search.js
+++ searching/1_linear_search.js
@@ -1,3 +1,15 @@
-function linear_search(){
-    
+function linear_search(arr, target){
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]==target){
+            return i;
+        }
+    }
+    // if data not found in list
+    return -1;
 }
+
+const numbers = [10, 20, 30, 40, 50];
+const target = 50;
+const result = linear_search(numbers, target);
+
+console.log(result !== -1 ? `Element found at index ${result}` : "Element not found");
