f3053a609086493b80c063d1999338f6df3df308
Hadi Gunawan Tambunan <hadigunawantambunan@JKT-MAC-2388.local>
2025-02-27 14:35:06 +0700

    add binary search

---
+++ searching/2_binary_search.js
@@ -0,0 +1,14 @@
+// using recursive
+function binary_search(arr, left, right, target){
+    
+    if (left > right) return -1;
+
+    let mid = Math.floor((left + right) / 2);
+    
+    if (arr[mid] === target) return mid;
+    if (arr[mid] < target) return binary_search(arr, mid + 1, right, target);
+    return binary_search(arr, left, mid - 1, target);
+}
+
+let arr = [10, 20, 30, 40, 50, 60];
+console.log(binary_search(arr, 0, arr.length - 1, 40)); 
